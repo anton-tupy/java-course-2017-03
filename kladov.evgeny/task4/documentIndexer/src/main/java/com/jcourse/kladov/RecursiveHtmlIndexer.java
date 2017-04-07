7296063958a71dbf91e0f20f225620739becc066
Kladov Eugene <kladov-eugene@ac-sw.com>
2017-04-07 08:44:00 +0700

    Use composition instead of inheritance in RecursiveHtmlIndexer.

--- kladov.evgeny/task4/documentIndexer/src/main/java/com/jcourse/kladov/RecursiveHtmlIndexer.java
+++ kladov.evgeny/task4/documentIndexer/src/main/java/com/jcourse/kladov/RecursiveHtmlIndexer.java
@@ -5,27 +5,36 @@ import lombok.extern.log4j.Log4j;
 import java.io.IOException;
 
 @Log4j
-public class RecursiveHtmlIndexer extends Indexer {
+public class RecursiveHtmlIndexer {
 	static final private int MAX_DEPTH = 1;
+	private Indexer indexer;
 
 	public RecursiveHtmlIndexer(Metric[] metrics) {
-		super(metrics);
+		indexer = new Indexer(metrics);
 	}
 
 	public void processHtmlDocument(String url) throws IOException {
-		processHtmlDocumentRecursive(url, 0);
+		try {
+			processHtmlDocumentRecursive(url, 0);
+		} catch (org.jsoup.UnsupportedMimeTypeException e) {
+			log.warn("UnsupportedMimeTypeException", e);
+		}
 	}
 
 	private void processHtmlDocumentRecursive(String url, int depth) throws IOException {
 		log.info("Fetching " + url);
 		HtmlDocument htmlDocument = new HtmlDocument(url);
 		log.info("Processing " + url);
-		super.processDocument(htmlDocument);
+		indexer.processDocument(htmlDocument);
 
 		if (depth >= MAX_DEPTH)
 			return;
 
 		for (String link : htmlDocument.getLinks())
 			processHtmlDocumentRecursive(link, depth + 1);
+	}
+
+	public void serializeMetrics(MetricSerializer serializer) {
+		indexer.serializeMetrics(serializer);
 	}
 }
